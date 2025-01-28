09545855aee9fe64e72bf5f359ffd72e4630053d
Glitzypanic <jose.fariasdv@gmail.com>
2025-01-28 01:53:13 -0400

    Add framer-motion dependency and enhance responsive styles for various components

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,91 +1,76 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { useRef, useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
-  const navRef = useRef();
-  const [isScrolled, setIsScrolled] = useState(false);
-  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-      if (scrollTop > 100) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const showNavbar = () => {
-    if (navRef.current) {
-      navRef.current.classList.toggle(styles.responsiveNav);
-    }
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
-  const isActive = (path) => {
-    return location.pathname == path ? styles.active : "";
+  const closeMenu = () => {
+    setIsMenuOpen(false);
   };
 
   return (
-    <header className={styles.navbar}>
-      <Link to="/">
-        <img src="/logo-imperio1.png" alt="" height={140} width={120} />
-      </Link>
-      <nav
-        ref={navRef}
-        className={`${isScrolled ? styles.navbarHidden : ""}`}
-        role="navigation"
-      >
-        <ul>
-          <li>
-            <Link to="/" className={isActive("/")}>
-              Inicio
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" className={isActive("/about")}>
-              Quiénes somos
-            </Link>
-          </li>
-          <li>
-            <Link to="/news" className={isActive("/news")}>
-              Noticias
-            </Link>
-          </li>
-          {/* <li>
-            <Link to="/resources" className={isActive("/resources")}>
-              Recursos
-            </Link>
-          </li> */}
-          {/* <li>
-            <Link to="/contact" className={isActive("/contact")}>
-              Contacto
-            </Link>
-          </li> */}
-        </ul>
-        <button
-          className={`${styles.navBtn} ${styles.navCloseBtn}`}
-          onClick={showNavbar}
-          aria-label="Cerrar menú"
+    <>
+      <header className={styles.navbar}>
+        <Link to="/">
+          <img src="/logo-imperio1.png" alt="Logo" height={140} width={120} />
+        </Link>
+        <motion.nav
+          initial={{ y: "-100%" }} // Posición inicial fuera de la vista
+          animate={{ y: isMenuOpen ? "0%" : "-100%" }} // Anima a la vista si está abierto
+          transition={{ type: "spring", stiffness: 260, damping: 20 }} // Tipo de transición
+          className={`${styles.nav} ${isMenuOpen ? styles.responsiveNav : ""}`}
+          style={{ backgroundColor: "#1c5f9c", color: "white" }}
+          role="navigation"
         >
-          <FaTimes />
+          <ul>
+            <li>
+              <Link to="/" onClick={closeMenu}>
+                Inicio
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" onClick={closeMenu}>
+                Quiénes somos
+              </Link>
+            </li>
+            <li>
+              <Link to="/news" onClick={closeMenu}>
+                Noticias
+              </Link>
+            </li>
+            {/* Puedes añadir más enlaces aquí */}
+          </ul>
+          <button
+            className={`${styles.navBtn} ${styles.navCloseBtn}`}
+            onClick={toggleMenu}
+            aria-label="Cerrar menú"
+          >
+            <FaTimes />
+          </button>
+        </motion.nav>
+        <button
+          className={styles.navBtn}
+          onClick={toggleMenu}
+          aria-label="Abrir menú"
+        >
+          <FaBars />
         </button>
-      </nav>
-      <button
-        className={styles.navBtn}
-        onClick={showNavbar}
-        aria-label="Abrir menú"
-      >
-        <FaBars />
-      </button>
-    </header>
+      </header>
+      <motion.div
+        className={`${styles.overlay} ${isMenuOpen ? styles.showOverlay : ""}`}
+        onClick={toggleMenu}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: isMenuOpen ? 0.1 : 0 }}
+        transition={{ duration: 0.1 }}
+      ></motion.div>
+    </>
   );
 }
 
