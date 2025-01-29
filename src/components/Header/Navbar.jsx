8de6104029d032908a20771cf4fe96114ccdef1e
Glitzypanic <jose.fariasdv@gmail.com>
2025-01-28 22:19:01 -0400

    Refactor responsive padding styles for main content and Navbar component

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -47,51 +47,48 @@ function Navbar() {
         >
           <FaBars />
         </button>
-        <motion.div
-          className={`${styles.overlay} ${
-            isMenuOpen ? styles.showOverlay : ""
-          }`}
-          onClick={toggleMenu}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: isMenuOpen ? 1 : 0 }}
-          transition={{ duration: 0.1 }}
-        >
-          <motion.nav
-            className={`${styles.nav} ${
-              isMenuOpen ? styles.responsiveNav : ""
-            }`}
-            style={{ backgroundColor: "#1c5f9c", color: "white" }}
-            role="navigation"
-            initial={{ x: "100%" }}
-            animate={{ x: isMenuOpen ? 0 : "100%" }}
-            transition={{ duration: 0.3 }}
+        {isMenuOpen && (
+          <motion.div
+            className={`${styles.overlay} ${styles.showOverlay}`}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.1 }}
           >
-            <ul>
-              <li>
-                <Link to="/" onClick={closeMenu}>
-                  Inicio
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" onClick={closeMenu}>
-                  Quiénes somos
-                </Link>
-              </li>
-              <li>
-                <Link to="/news" onClick={closeMenu}>
-                  Noticias
-                </Link>
-              </li>
-            </ul>
-            <button
-              className={`${styles.navBtn} ${styles.navCloseBtn}`}
-              onClick={toggleMenu}
-              aria-label="Cerrar menú"
+            <motion.nav
+              className={`${styles.nav} ${styles.responsiveNav}`}
+              style={{ backgroundColor: "#1c5f9c", color: "white" }}
+              role="navigation"
+              initial={{ x: "100%" }}
+              animate={{ x: 0 }}
+              transition={{ duration: 0.3 }}
             >
-              <FaTimes />
-            </button>
-          </motion.nav>
-        </motion.div>
+              <ul>
+                <li>
+                  <Link to="/" onClick={closeMenu}>
+                    Inicio
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about" onClick={closeMenu}>
+                    Quiénes somos
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/news" onClick={closeMenu}>
+                    Noticias
+                  </Link>
+                </li>
+              </ul>
+              <button
+                className={`${styles.navBtn} ${styles.navCloseBtn}`}
+                onClick={closeMenu}
+                aria-label="Cerrar menú"
+              >
+                <FaTimes />
+              </button>
+            </motion.nav>
+          </motion.div>
+        )}
       </header>
     </>
   );
